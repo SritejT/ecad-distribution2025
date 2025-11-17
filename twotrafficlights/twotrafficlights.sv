e09afe652634e5a8af8ebb716e81c19cb16b732b
Sritej Tummuru <sritej.tummuru@gmail.com>
2025-11-17 11:03:21 +0000

    Finished ticks 1 and 2

--- twotrafficlights/twotrafficlights.sv
+++ twotrafficlights/twotrafficlights.sv
@@ -5,6 +5,54 @@ module twotrafficlights(
       output logic [2:0] lightsB
     );
   logic [2:0] state;
-  // insert your code here
+  
+  always_ff @(posedge clk or posedge rst)
+   begin
+      if (rst)
+        state <= 0;
+      else
+        state <= state + 1;
+   end
+
+
+  always_comb
+    begin
+      case (state)
+        3'd0: begin
+          lightsA = 3'd4;
+          lightsB = 3'd6;
+        end
+        3'd1: begin
+          lightsA = 3'd4;
+          lightsB = 3'd1;
+        end
+        3'd2: begin
+          lightsA = 3'd4;
+          lightsB = 3'd2;
+        end
+        3'd3: begin
+          lightsA = 3'd4;
+          lightsB = 3'd4;
+        end
+        3'd4: begin
+          lightsA = 3'd6;
+          lightsB = 3'd4;
+        end
+        3'd5: begin
+          lightsA = 3'd1;
+          lightsB = 3'd4;
+        end
+        3'd6: begin
+          lightsA = 3'd2;
+          lightsB = 3'd4;
+        end
+        default: begin
+          lightsA = 3'd4;
+          lightsB = 3'd4;
+        end
+      endcase
+    end
+
 
 endmodule
+
