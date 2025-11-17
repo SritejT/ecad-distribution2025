5a1569f5cad1a778d98e6cb3acb29ff2b56dfbf9
SritejT <srt52@cam.ac.uk>
2025-11-17 15:47:36 +0000

    Finished tick 3.

--- riscv/assembly/src/div.s
+++ riscv/assembly/src/div.s
@@ -14,14 +14,65 @@ div:
     # ...
 
     # do your work
-    # example of printing inputs a0 and a1
-    DEBUG_PRINT a0
-    DEBUG_PRINT a1
 
-    # load every register you stored above
-    lw   ra, 28(sp)
-    lw   s0, 24(sp)
-    # ...
-    addi sp, sp, 32      # Free up stack space
-    ret
+    beq x0, a1, zerofinish
+
+    li a5, 0 # Q
+    li a6, 0 # R
+
+    li a3, 1 # Current power of 2
+    li a2, 0 # Number of bits in N
+
+    log:
+        bltu a0, a3, algsetup
+        slli a3, a3, 1
+        addi a2, a2, 1
+        j log
+
+    algsetup:
+        addi a2, a2, -1
+        j alg
+
+    alg:
+        blt a2, x0, finish
+
+        slli a6, a6, 1
+
+        srl t1, a0, a2
+        andi t1, t1, 1
+        add a6, a6, t1
+
+        bgeu a6, a1, cond
+
+        addi a2, a2, -1
+
+        j alg
+        
+    cond:
+        sub a6, a6, a1
+
+        li t3, 1
+        sll t3, t3, a2
+        or a5, a5, t3
+
+        addi a2, a2, -1
+
+        j alg
+
+    finish:
+        mv a0, a5
+        mv a1, a6
+        j cleanup
+
+    zerofinish: 
+        li a0, 0
+        j cleanup
+
+    cleanup:
+        # load every register you stored above
+        lw   ra, 28(sp)
+        lw   s0, 24(sp)
+        # ...
+        addi sp, sp, 32      # Free up stack space
+        ret
 
